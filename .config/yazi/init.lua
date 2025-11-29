fc8abb572bdb726a42ae574f7270d48004ce4f2c
amin-dev-tech <amin.dev.tech@gmail.com>
2025-11-30 00:57:53 +0300

    add more configs

---
+++ .config/yazi/init.lua
@@ -0,0 +1,40 @@
+require("yatline"):setup({
+	show_background = true,
+
+	header_line = {
+		left = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {},
+			section_b = {},
+			section_c = {},
+		},
+	},
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_name" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "cursor_percentage" },
+			},
+			section_c = {
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+})
