45d4fb4d14901539aa3b7b88974a36cf6d25dd90
amin-dev-tech <amin.dev.tech@gmail.com>
2025-11-29 16:19:30 +0300

    working on startup performance - part2

--- .config/nvim/lua/Amin/plugins/windsurf.lua
+++ .config/nvim/lua/Amin/plugins/windsurf.lua
@@ -3,7 +3,7 @@ return {
 	event = "VeryLazy",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
+		-- "hrsh7th/nvim-cmp",
 	},
 	config = function()
 		require("codeium").setup({
